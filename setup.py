b61505c477d227ebbccfffcd2870065abcea819e
Hebanguher <gh22026@ues.edu.sv>
2024-11-15 23:47:43 -0600

    requerimientos

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lessa_lib',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Librería para reconocimiento de lenguaje de señas',
+    install_requires=[
+        'opencv-python',
+        'mediapipe',
+        'numpy',
+        'tensorflow',
+        'scikit-learn',
+        'pyttsx3',
+    ],
+)
